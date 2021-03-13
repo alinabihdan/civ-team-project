2251edd817c00405e0b4d3c41adbd2f5185a0321
alinabihdan <alina.bigdan1@gmail.com>
2021-03-13 21:23:15 +0200

    добавляю немного джса

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,5 +1,8 @@
 var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
     pagination: {
       el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
     },
   });
