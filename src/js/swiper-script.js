2accd6ec1bd4e98b1019102fbfefb0d5402bddd8
Sergey-Markov <1nativefl@gmail.com>
2021-03-13 12:09:35 +0200

    1

--- src/js/swiper-script.js
+++
@@ -1,9 +0,0 @@
-new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true,
-    },
-});
